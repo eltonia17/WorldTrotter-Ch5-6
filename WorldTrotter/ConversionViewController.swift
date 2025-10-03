9f37e5d76a66521b9a2ac2ff663765e79fbef462
Leonard <esl107@miami.edu>
2025-10-03 19:54:42 -0400

    Chapter 5 Bronze and Silver challenges

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -6,7 +6,9 @@
 //
 import UIKit
 
-class ConversionViewController: UIViewController {
+final class ConversionViewController: UIViewController {
+
+    // change color on first load + every time the tab appears
     override func viewDidLoad() {
         super.viewDidLoad()
         setRandomBackground()
@@ -17,6 +19,57 @@ class ConversionViewController: UIViewController {
         setRandomBackground()
     }
 
+    override func loadView() {
+        // root view
+        let root = UIView()
+        root.backgroundColor = .systemBackground
+        self.view = root
+
+        // labels (big numbers and text rows)
+        let fValue = makeLabel("212", font: .systemFont(ofSize: 96, weight: .bold))
+        let fText  = makeLabel("degrees Fahrenheit", font: .preferredFont(forTextStyle: .title2))
+
+        let isReally = makeLabel("is really", font: .preferredFont(forTextStyle: .title3))
+
+        let cValue = makeLabel("100", font: .systemFont(ofSize: 96, weight: .bold))
+        let cText  = makeLabel("degrees Celsius", font: .preferredFont(forTextStyle: .title2))
+
+        // vertical stack to lay them out
+        let stack = UIStackView(arrangedSubviews: [fValue, fText, isReally, cValue, cText])
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 8
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        root.addSubview(stack)
+
+        // center the stack and respect safe area/margins
+        NSLayoutConstraint.activate([
+            stack.centerXAnchor.constraint(equalTo: root.layoutMarginsGuide.centerXAnchor),
+            stack.centerYAnchor.constraint(equalTo: root.safeAreaLayoutGuide.centerYAnchor),
+            stack.leadingAnchor.constraint(greaterThanOrEqualTo: root.layoutMarginsGuide.leadingAnchor),
+            stack.trailingAnchor.constraint(lessThanOrEqualTo: root.layoutMarginsGuide.trailingAnchor)
+        ])
+
+        // wrap body labels and center them
+        [fText, isReally, cText].forEach {
+            $0.numberOfLines = 0
+            $0.textAlignment = .center
+        }
+    }
+
+    // small helper to create labels
+    private func makeLabel(_ text: String, font: UIFont) -> UILabel {
+        let l = UILabel()
+        l.text = text
+        l.font = font
+        l.textColor = .label
+        l.textAlignment = .center
+        l.adjustsFontForContentSizeCategory = true
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }
+
+    // random background color
     private func setRandomBackground() {
         view.backgroundColor = UIColor(
             hue: CGFloat.random(in: 0...1),

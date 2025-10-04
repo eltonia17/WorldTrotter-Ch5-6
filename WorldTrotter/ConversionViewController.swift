0be2086d893981a40183d137a9ddb3d1ae7b8c3a
Leonard <esl107@miami.edu>
2025-10-03 20:55:15 -0400

    Chapter 5 +  Chapter 6 responding to text field changes

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -4,3 +4,18 @@
 //
 //  Created by Eltonia Leonard on 9/20/25.
 //
+import UIKit
+
+final class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("ConversionViewController loaded its view.")
+    }
+
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        celsiusLabel.text = textField.text
+    }
+}
